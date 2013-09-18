c26c43cbe523ea46754e906cffebf4e5203cf4e0
ronenhamias <ronen.hamias@gmail.com>
2013-09-18 22:24:00 +0300

    refactor the arena

---
+++ WebContent/game/Arena.js
@@ -0,0 +1,19 @@
+function Arena(world){
+	// create the ground
+	tQuery.createCube(1,0.2,1, 3,3,3).addTo(world)
+		.receiveShadow(true)
+		.geometry().smooth(2).back()
+		.scale(3*1.5, 1, 1.8*1.5)
+		.translateY(-0.2-0.1)
+		.setLambertMaterial().map('images/13663tennis_court_grass.jpg').ambient(0x444444).back();
+
+	// lights
+	tQuery.createAmbientLight().addTo(world).color(0x444444);
+	tQuery.createPointLight().addTo(world).intensity(1).distance(30);
+	tQuery.createDirectionalLight().addTo(world)
+		.position(2, 4, -1.5).color(0xffffff)
+		.castShadow(true)
+		.shadowCamera(2, -2, 2, -2, 0.01, 200)
+		.shadowDarkness(0.7).shadowBias(.002);
+		//.shadowCameraVisible(true);
+};
--- WebContent/game/game.js
+++ WebContent/game/game.js
@@ -10,27 +10,7 @@ function Game(){
 	smokepuff	= new Fireworks.ComboEmitter.Smokepuff();
 	tQuery(smokepuff.object3D()).addTo(world).scale(1/10);
 	smokepuff.sound().volume(0.3);
-
-	
-
-	// create the ground
-	tQuery.createCube(1,0.2,1, 3,3,3).addTo(world)
-		.receiveShadow(true)
-		.geometry().smooth(2).back()
-		.scale(3*1.5, 1, 1.8*1.5)
-		.translateY(-0.2-0.1)
-		.setLambertMaterial().map('images/13663tennis_court_grass.jpg').ambient(0x444444).back();
-
-	// lights
-	tQuery.createAmbientLight().addTo(world).color(0x444444);
-	tQuery.createPointLight().addTo(world).intensity(1).distance(30);
-	tQuery.createDirectionalLight().addTo(world)
-		.position(2, 4, -1.5).color(0xffffff)
-		.castShadow(true)
-		.shadowCamera(2, -2, 2, -2, 0.01, 200)
-		.shadowDarkness(0.7).shadowBias(.002);
-		//.shadowCameraVisible(true);
-
+	arena = new Arena(world);
 	players = new Players(world);
 	
 	
